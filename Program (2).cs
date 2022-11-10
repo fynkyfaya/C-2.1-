3d60cf5c5537e4bbc498e6898da097876e911e0e
Фадеев Тимур Сергеевич <110234293+fynkyfaya@users.noreply.github.com>
2022-11-10 20:22:12 +0200

    Add files via upload

---
+++ Program (2).cs
@@ -0,0 +1,12 @@
+﻿Console.Write("X value: ");
+var x = double.Parse(Console.ReadLine());
+
+Console.Write("Y value: ");
+var y = double.Parse(Console.ReadLine());
+
+
+if (y >= x && y >= -x && y <= 1) {
+    Console.WriteLine("Точка входит в область");
+} else {
+    Console.WriteLine("Точка не входит в область");
+}
